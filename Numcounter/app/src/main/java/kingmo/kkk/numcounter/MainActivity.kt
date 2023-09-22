e4571590e123f04e7e22d59a9a1a35af68c9b33a
kkingmo <55ooccmm@gmail.com>
2023-09-22 11:51:33 +0900

    sample-app : 숫자 카운터

--- Numcounter/app/src/main/java/kingmo/kkk/numcounter/MainActivity.kt
+++ Numcounter/app/src/main/java/kingmo/kkk/numcounter/MainActivity.kt
@@ -12,24 +12,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         /**
-         * 여기서 `findViewById` 메서드를 사용하여 XML 레이아웃 파일에서 정의된 뷰들을 참조합니다.
-         * 이 메서드를 통해 뷰의 인스턴스에 접근할 수 있게 되어, 이후에 이 뷰들을 조작할 수 있게 됩니다.
+         * `findViewById`를 사용하여 레이아웃에 정의된 뷰를 참조합니다.
+         * 이 예제에서는 TextView와 두 개의 Button을 참조하고 있습니다.
+         * 이를 통해 사용자 인터페이스의 상태를 관리하고 업데이트할 수 있습니다.
          */
         val numberTextView = findViewById<TextView>(R.id.numberTextView)
         val resetButton = findViewById<Button>(R.id.resetButton)
         val plusButton = findViewById<Button>(R.id.plusButton)
 
+        var number = 0
+
         /**
-         * `setOnClickListener` 메서드를 사용하여 버튼에 클릭 리스너를 설정합니다.
-         * 이 리스너는 버튼이 클릭될 때 실행될 코드 블록을 정의합니다.
-         * 여기서는 버튼들이 클릭되면 로그 메시지를 출력합니다.
+         * `resetButton`에 `setOnClickListener`를 설정하여, 버튼이 클릭되면 `number`를 0으로 초기화하고
+         * `numberTextView`의 텍스트를 업데이트합니다.
+         * 이를 통해 사용자에게 현재 상태가 0임을 보여줍니다.
          */
         resetButton.setOnClickListener {
-            Log.d("onClick", "리셋 버튼이 클릭 됐습니다.")
+            number = 0
+            numberTextView.text = number.toString()
+            Log.d("onClick", "number $number")
         }
 
+        /**
+         * `plusButton`에도 `setOnClickListener`를 설정하여, 버튼이 클릭되면 `number`를 1 증가시키고
+         * `numberTextView`의 텍스트를 업데이트합니다.
+         * 이를 통해 사용자에게 현재 상태가 증가했음을 보여줍니다.
+         */
         plusButton.setOnClickListener {
-            Log.i("onClick", "플러스 버튼이 클릭 됐습니다")
+            number += 1
+            numberTextView.text = number.toString()
+            Log.i("onClick", "number $number")
         }
     }
 }
