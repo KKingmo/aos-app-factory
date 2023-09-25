efd9cb4fc92ec00019602883be72a74f1c244cbd
kkingmo <55ooccmm@gmail.com>
2023-09-25 17:36:10 +0900

    데이터 저장, 불러오기

--- EmergencyInfo/app/src/main/java/kingmo/kkk/emergencyinformation/EditActivity.kt
+++ EmergencyInfo/app/src/main/java/kingmo/kkk/emergencyinformation/EditActivity.kt
@@ -1,18 +1,96 @@
 package kingmo.kkk.emergencyinformation
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.view.isVisible
+import kingmo.kkk.emergencyinformation.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /**
+         * 'binding.bloodTypeSpinner.adapter'를 통해 'bloodTypeSpinner'에 어댑터를 설정합니다.
+         * ArrayAdapter.createFromResource를 사용하여 blood_types 배열을 가져와 어댑터를 생성하며,
+         * 이를 통해 사용자에게 혈액형을 선택할 수 있는 드롭다운 목록을 제공합니다.
+         */
+        binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(
+            this, R.array.blood_types, android.R.layout.simple_list_item_1
+        )
 
         /**
-         * MainActivity에서 전달된 메시지를 "intentMessage" 키를 사용하여 가져옵니다.
+         * 'birthdateLayer' 클릭 시, 사용자에게 날짜를 선택할 수 있는 DatePickerDialog를 표시합니다.
+         * 사용자가 날짜를 선택하면, 선택된 날짜가 'birthdateValueTextView'에 표시됩니다.
          */
-        val message = intent.getStringExtra("intentMessage") ?: "없음"
-        Log.d("intentMessage", message)
+        binding.birthdateLayer.setOnClickListener {
+            val listener = OnDateSetListener { _, year, month, dayOfMonth ->
+                binding.birthdateValueTextView.text = "$year-${month.inc()}-$dayOfMonth"
+            }
+            DatePickerDialog(
+                this, listener, 2000, 1, 1
+            ).show()
+        }
+
+        /**
+         * 'warningCheckBox'의 체크 상태에 따라 'warningEditText'의 가시성을 변경합니다.
+         */
+        binding.warningCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            binding.warningEditText.isVisible = isChecked
+        }
+
+        binding.warningEditText.isVisible = binding.warningCheckBox.isChecked
+
+        /**
+         * 'saveButton' 클릭 시, 사용자의 정보를 저장하고 액티비티를 종료합니다.
+         */
+        binding.saveButton.setOnClickListener {
+            saveData()
+            finish()
+        }
     }
+
+    /**
+     * 'saveData' 함수는 사용자의 정보를 Shared Preferences에 저장합니다.
+     * 이 함수는 사용자의 이름, 혈액형, 비상 연락처, 생년월일, 경고 메시지를 저장합니다.
+     * 저장이 완료되면, 사용자에게 토스트 메시지로 저장 완료를 알립니다.
+     */
+    private fun saveData() {
+        with(getSharedPreferences("userInformation", Context.MODE_PRIVATE).edit()) {
+            putString(NAME, binding.nameEditText.text.toString())
+            putString(BLOOD_TYPE, getBloodType())
+            putString(EMERGENCY_CONTACT, binding.emergencyContactEditText.text.toString())
+            putString(BIRTHDATE, binding.birthdateTextView.text.toString())
+            putString(WARNING, getWarning())
+            apply() // apply는 commit과 달리 비동기로 처리하기 때문에 사용자의 동작을 막지 않습니다.
+        }
+
+        Toast.makeText(this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show() // 알림 토스트
+    }
+
+    /**
+     * 'getBloodType' 함수는 선택된 혈액형을 반환합니다.
+     * 이 함수는 'bloodTypeSpinner'에서 선택된 항목과 'bloodTypePlus' 체크박스의 상태를 조합하여 결과를 생성합니다.
+     */
+    private fun getBloodType(): String {
+        val bloodAlphabet = binding.bloodTypeSpinner.selectedItem.toString()
+        val bloodSign = if (binding.bloodTypePlus.isChecked) "+" else "-"
+        return "$bloodSign$bloodAlphabet"
+    }
+
+    /**
+     * 'getWarning' 함수는 'warningCheckBox'가 체크되어 있을 경우 'warningEditText'의 텍스트를 반환합니다.
+     * 체크되어 있지 않은 경우, 빈 문자열을 반환합니다.
+     */
+    private fun getWarning(): String {
+        return if (binding.warningCheckBox.isChecked) binding.warningEditText.text.toString() else ""
+    }
+
 }
--- EmergencyInfo/app/src/main/java/kingmo/kkk/emergencyinformation/MainActivity.kt
+++ EmergencyInfo/app/src/main/java/kingmo/kkk/emergencyinformation/MainActivity.kt
@@ -1,8 +1,10 @@
 package kingmo.kkk.emergencyinformation
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import kingmo.kkk.emergencyinformation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,17 +15,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         /**
-         * 'goInputActivityButton'이 클릭되면, EditActivity로 이동하기 위한 Intent가 생성됩니다.
-         * 또한, "응급의료정보"라는 메시지를 putExtra 메서드를 사용하여 EditActivity로 전달합니다.
-         * 여기서 'binding.goInputActivityButton.setOnClickListener'는 'goInputActivityButton' 버튼이 클릭되었을 때 수행될 동작을 정의합니다.
-         * 'Intent(this, EditActivity::class.java)'는 명시적 Intent를 생성합니다. 이 Intent는 현재 액티비티에서 EditActivity로의 전환을 나타냅니다.
-         * 'intent.putExtra("intentMessage","응급의료정보")'는 Intent에 추가 데이터를 삽입합니다. 이 데이터는 "intentMessage"라는 키로 EditActivity에서 접근 가능합니다.
-         * 'startActivity(intent)'는 생성된 Intent를 사용하여 EditActivity를 시작합니다.
+         * 'goInputActivityButton' 클릭 시, EditActivity로 이동합니다.
+         * 이를 위해 명시적 Intent를 생성하고 startActivity를 호출하여 EditActivity를 시작합니다.
          */
         binding.goInputActivityButton.setOnClickListener {
             val intent = Intent(this, EditActivity::class.java) // 명시적 Intent 생성
-            intent.putExtra("intentMessage","응급의료정보") // "응급의료정보" 메시지를 Intent에 추가
             startActivity(intent) // EditActivity 시작
+        }
+    }
+
+    /**
+     * 'onResume' 메서드는 액티비티가 사용자와 상호작용하기 직전에 호출됩니다.
+     * 이 메서드에서 'getDataUiUpdate' 함수를 호출하여 UI를 최신 상태로 업데이트합니다.
+     * 이는 사용자가 EditActivity에서 데이터를 변경한 후 다시 MainActivity로 돌아왔을 때 변경사항을 반영하기 위함입니다.
+     */
+    override fun onResume() {
+        super.onResume()
+        getDataUiUpdate()
+    }
+
+    /**
+     * 'getDataUiUpdate' 함수는 Shared Preferences로부터 사용자 정보를 읽어와 UI를 업데이트합니다.
+     * 각 TextView는 해당하는 데이터가 없을 경우 "미정"이라는 기본값을 표시합니다.
+     * 또한, 경고 메시지가 있을 경우에만 경고 메시지 관련 TextView를 표시합니다.
+     */
+    private fun getDataUiUpdate() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)) {
+            binding.nameValueTextView.text = getString(NAME, "미정")
+            binding.birthdateValueTextView.text = getString(BIRTHDATE, "미정")
+            binding.bloodTypeValueTextView.text = getString(BLOOD_TYPE, "미정")
+            binding.emergencyContactValueTextView.text = getString(EMERGENCY_CONTACT, "미정")
+            val warning = getString(WARNING, "")
+
+            binding.warningTextView.isVisible = warning.isNullOrEmpty().not()
+            binding.warningValueTextView.isVisible = warning.isNullOrEmpty().not()
+
+            if (warning.isNullOrEmpty()) {
+                binding.warningValueTextView.text = warning
+            }
+
         }
     }
 }
