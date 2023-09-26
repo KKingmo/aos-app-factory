c3e538850e7182871cf01633af3bd4c65bc349d6
kkingmo <55ooccmm@gmail.com>
2023-09-26 18:51:08 +0900

    UI 스레드 외부에서 작업하고 안전하게 UI 반영하기

--- Stopwatch/app/src/main/java/kingmo/kkk/stopwatch/MainActivity.kt
+++ Stopwatch/app/src/main/java/kingmo/kkk/stopwatch/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import kingmo.kkk.stopwatch.databinding.ActivityMainBinding
 import kingmo.kkk.stopwatch.databinding.DialogCountdownSettingBinding
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var countdownSecond = 10
+    private var currentCountdownDeciSecond = countdownSecond * 10
+    private var currentDeciSecond = 0
+    private var timer: Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         /**
-         * 카운트다운 텍스트 뷰가 클릭될 때 카운트다운 설정 다이얼로그 표시
+         * 카운트다운 클릭 시 카운트다운 설정 다이얼로그 표시
          */
         binding.countdownTextView.setOnClickListener {
             showCountdownSettingDialog()
@@ -53,15 +58,53 @@ class MainActivity : AppCompatActivity() {
         }
 
         /**
-         * 랩 버튼 클릭 시 현재시간 기록
+         * todo 랩 버튼 클릭 시 현재시간 기록
          */
         binding.lapButton.setOnClickListener {
             lap()
         }
+        initCountdownViews()
     }
 
-    private fun start() {
+    /**
+     * 카운트다운 UI 초기화
+     */
+    private fun initCountdownViews() {
+        binding.countdownTextView.text = String.format("%02d", countdownSecond)
+        binding.countdownProgressBar.progress = 100
+    }
 
+
+    /**
+     * `timer { }`를 통해 작업자 스레드에서 작업을 실행합니다.
+     * 작업하고 Handler를 통해 UI에 반영하기 위해 `runOnUiThread`, `binding.root.post`같은 놈들을 사용 합니다.
+     */
+    private fun start() {
+        timer = timer(initialDelay = 0, period = 100) {
+            if (currentCountdownDeciSecond == 0) {
+                currentDeciSecond += 1
+
+                val minutes = currentDeciSecond.div(10) / 60
+                val seconds = currentDeciSecond.div(10) % 60
+                val deciSeconds = currentDeciSecond % 10
+
+                runOnUiThread {
+                    binding.timeTextView.text = String.format("%02d:%02d", minutes, seconds)
+                    binding.tickTextView.text = deciSeconds.toString()
+
+                    binding.countdownGroup.isVisible = false
+                }
+            } else {
+                currentCountdownDeciSecond -= 1
+                val seconds = currentCountdownDeciSecond / 10
+                val progress = (currentCountdownDeciSecond / (countdownSecond * 10f)) * 100
+
+                binding.root.post {
+                    binding.countdownTextView.text = String.format("%02d", seconds)
+                    binding.countdownProgressBar.progress = progress.toInt()
+                }
+            }
+        }
     }
 
     /**
@@ -72,10 +115,22 @@ class MainActivity : AppCompatActivity() {
         binding.stopButton.isVisible = true
         binding.pauseButton.isVisible = false
         binding.lapButton.isVisible = false
+
+        currentDeciSecond = 0
+        binding.timeTextView.text = "00:00"
+        binding.tickTextView.text = "0"
+
+        binding.countdownGroup.isVisible = true
+        initCountdownViews()
     }
 
+    /**
+     * 스톱워치 일시정지
+     * 경과시간은 currentDeciSecond에 저장됩니다.
+     */
     private fun pause() {
-
+        timer?.cancel()
+        timer = null
     }
 
     private fun lap() {
@@ -97,6 +152,7 @@ class MainActivity : AppCompatActivity() {
             setView(dialogBinding.root)
             setPositiveButton("확인") { _, _ ->
                 countdownSecond = dialogBinding.countdownSecondPicker.value
+                currentCountdownDeciSecond = countdownSecond * 10
                 binding.countdownTextView.text = String.format("%02d", countdownSecond)
             }
             setNegativeButton("취소", null)
@@ -116,3 +172,36 @@ class MainActivity : AppCompatActivity() {
         }.show()
     }
 }
+
+/**
+ * 안드로이드 스레드 이해 :
+ * - 안드로이드 앱은 하나 이상의 스레드로 구성되고, 스레드는 프로세스 내에서 실행되는 독립적인 실행흐름입니다.
+ * - 각 스레드는 자체 메모리 스택과 실행 스택을 갖습니다.
+ *
+ * UI 스레드 :
+ * - 앱의 UI를 업데이트에 사용되는 스레드입니다.
+ * - Android 시스템에 의해 생성되며, 일반적으로 메인 스레드라고 불립니다.
+ * - UI 스레드에서만 UI를 조작해야하며, UI 스레드를 차단하면 UI가 응답하지 않습니다.
+ * - UI 스레드를 차단하는 작업 :
+ *   - 네트워크 요청, 데이터베이스 쿼리, 파일 I/O
+ *
+ * 작업자 스레드 :
+ * - UI 스레드와 별개로 실행되는 스레드로 CPU 많이쓰는 작업이나 UI 스레드를 차단할 수 있는 작업을 수행하는 데 사용됩니다.
+ * - 작업자 스레드에서 UI 스레드로 UI 업데이트 하려면 Handler를 사용해야 합니다.
+ *
+ * UI 스레드를 차단하지 않도록 해야하는 이유 :
+ * - UI 스레드를 차단하면 UI가 응답하지 않아 사용자는 앱이 고장났다고 느끼게 됩니다.
+ * - UI 스레드가 5초 이상 차단 되었을 때 ANR(Application Not Responding) 오류가 발생하여 앱이 강제 종료될 수 있습니다.
+ *
+ * UI 스레드를 차단하지 않는 방법 :
+ * - UI는 UI 스레드에서만 조작합니다.
+ * - 작업자 스레드에서 UI 스레드로 UI를 업데이트 하려면 Handler를 사용합시다.
+ *
+ * Handler 사용 방법 :
+ * - Handler를 사용하여 작업자 스레드에서 UI 스레드로 메시지를 보내거나, 반대로 UI 스레드에서 작업자 스레드로 작업을 요청할 수 있습니다.
+ * - Handler는 메시지 큐를 사용하여 UI 스레드와 작업합니다.
+ * - 메시지 큐에 작업이 등록되면 Looper라는 놈이 메시지 큐의 작업들을 가져와 UI 스레드에서 처리되도록 합니다.
+ *
+ * 결론 :
+ * UI 조작은 UI 스레드에서만 합시다. UI 스레드 외부에서 작업하고 UI에 반영할 때는 Handler 사용 합시다.
+ */
