173e1b5d36011cac8ff81a0f6cca27efa02f7314
kkingmo <55ooccmm@gmail.com>
2023-10-05 18:07:16 +0900

    LowBatteryReceiver 구현

--- MusicPlayer/app/src/main/java/kingmo/kkk/musicplayer/MediaPlayerService.kt
+++ MusicPlayer/app/src/main/java/kingmo/kkk/musicplayer/MediaPlayerService.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.drawable.Icon
 import android.media.MediaPlayer
 import android.os.IBinder
@@ -13,6 +14,7 @@ import android.os.IBinder
 class MediaPlayerService : Service() {
 
     private var mediaPlayer: MediaPlayer? = null
+    private val receiver = LowBatteryReceiver()
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
@@ -20,6 +22,7 @@ class MediaPlayerService : Service() {
     override fun onCreate() {
         super.onCreate()
         createNotificationChannel()
+        initReceiver()
 
         val playIcon = Icon.createWithResource(baseContext, R.drawable.round_play_arrow_24)
         val pauseIcon = Icon.createWithResource(baseContext, R.drawable.round_pause_24)
@@ -71,6 +74,13 @@ class MediaPlayerService : Service() {
         startForeground(100, notification)
     }
 
+    private fun initReceiver() {
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filter)
+    }
+
     private fun createNotificationChannel() {
         val channel =
             NotificationChannel(CHANNEL_ID, "MEDIA_PLAYER", NotificationManager.IMPORTANCE_DEFAULT)
@@ -108,6 +118,7 @@ class MediaPlayerService : Service() {
             release()
         }
         mediaPlayer = null
+        unregisterReceiver(receiver)
         super.onDestroy()
     }
 }
