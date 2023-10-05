9c68b358f6110898a1220f6a962088946d321c55
kkingmo <55ooccmm@gmail.com>
2023-10-05 15:38:12 +0900

    MediaPlayer play, pause, stop구현

--- MusicPlayer/app/src/main/java/kingmo/kkk/musicplayer/MainActivity.kt
+++ MusicPlayer/app/src/main/java/kingmo/kkk/musicplayer/MainActivity.kt
@@ -1,18 +1,40 @@
 package kingmo.kkk.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kingmo.kkk.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.playButton.setOnClickListener {  }
-        binding.pauseButton.setOnClickListener {  }
-        binding.stopButton.setOnClickListener {  }
+        binding.playButton.setOnClickListener { mediaPlayerPlay() }
+        binding.pauseButton.setOnClickListener { mediaPlayerPause() }
+        binding.stopButton.setOnClickListener { mediaPlayerStop() }
+    }
+
+    private fun mediaPlayerPlay() {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.bgm).apply {
+                isLooping = true
+            }
+        }
+        mediaPlayer?.start()
+    }
+
+    private fun mediaPlayerPause() {
+        mediaPlayer?.pause()
+    }
+
+    private fun mediaPlayerStop() {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
