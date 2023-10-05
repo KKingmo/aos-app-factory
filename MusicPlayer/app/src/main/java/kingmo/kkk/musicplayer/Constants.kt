c5caf3658480b643e57ff3e448560246089234f4
kkingmo <55ooccmm@gmail.com>
2023-10-05 16:08:03 +0900

    Service 사용해서 백그라운드 미디어재생

---
+++ MusicPlayer/app/src/main/java/kingmo/kkk/musicplayer/Constants.kt
@@ -0,0 +1,5 @@
+package kingmo.kkk.musicplayer
+
+const val MEDIA_PLAYER_PLAY = "play"
+const val MEDIA_PLAYER_PAUSE = "pause"
+const val MEDIA_PLAYER_STOP = "stop"
--- MusicPlayer/app/src/main/java/kingmo/kkk/musicplayer/MainActivity.kt
+++ MusicPlayer/app/src/main/java/kingmo/kkk/musicplayer/MainActivity.kt
@@ -1,5 +1,6 @@
 package kingmo.kkk.musicplayer
 
+import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,7 +9,6 @@ import kingmo.kkk.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var mediaPlayer: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,21 +20,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun mediaPlayerPlay() {
-        if (mediaPlayer == null) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.bgm).apply {
-                isLooping = true
-            }
-        }
-        mediaPlayer?.start()
+        val intent =
+            Intent(this, MediaPlayerService::class.java).apply { action = MEDIA_PLAYER_PLAY }
+        startService(intent)
     }
 
     private fun mediaPlayerPause() {
-        mediaPlayer?.pause()
+        val intent =
+            Intent(this, MediaPlayerService::class.java).apply { action = MEDIA_PLAYER_PAUSE }
+        startService(intent)
     }
 
     private fun mediaPlayerStop() {
-        mediaPlayer?.stop()
-        mediaPlayer?.release()
-        mediaPlayer = null
+        val intent =
+            Intent(this, MediaPlayerService::class.java).apply { action = MEDIA_PLAYER_STOP }
+        startService(intent)
     }
 }
