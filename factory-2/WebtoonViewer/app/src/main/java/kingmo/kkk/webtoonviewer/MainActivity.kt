2453e274d2315666418273c232e908854f96c383
kkingmo <55ooccmm@gmail.com>
2023-10-06 13:49:02 +0900

    Webview, Fragement 구성

--- factory-2/WebtoonViewer/app/src/main/java/kingmo/kkk/webtoonviewer/MainActivity.kt
+++ factory-2/WebtoonViewer/app/src/main/java/kingmo/kkk/webtoonviewer/MainActivity.kt
@@ -2,10 +2,29 @@ package kingmo.kkk.webtoonviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kingmo.kkk.webtoonviewer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainer, WebViewFragment())
+                commit()
+            }
+        }
+
+        binding.button2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainer, BFragment())
+                commit()
+            }
+        }
     }
 }
