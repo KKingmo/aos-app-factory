de0d67db667a645508b83738e2f952db871b74c4
kkingmo <55ooccmm@gmail.com>
2023-10-06 16:49:27 +0900

    마지막 시점 돌아가기 구현

--- factory-2/WebtoonViewer/app/src/main/java/kingmo/kkk/webtoonviewer/WebtoonWebViewClient.kt
+++ factory-2/WebtoonViewer/app/src/main/java/kingmo/kkk/webtoonviewer/WebtoonWebViewClient.kt
@@ -2,13 +2,22 @@ package kingmo.kkk.webtoonviewer
 
 import android.graphics.Bitmap
 import android.view.View
-import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ProgressBar
 
-class WebtoonWebViewClient(private val progressBar: ProgressBar) : WebViewClient() {
+class WebtoonWebViewClient(
+    private val progressBar: ProgressBar,
+    private val saveData: (String) -> Unit,
+) : WebViewClient() {
+
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        if (request != null && request.url.toString().contains("comic.naver.com/webtoon/detail")) {
+            saveData.invoke(request.url.toString())
+        }
+        return super.shouldOverrideUrlLoading(view, request)
+    }
 
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
@@ -20,14 +29,5 @@ class WebtoonWebViewClient(private val progressBar: ProgressBar) : WebViewClient
         super.onPageStarted(view, url, favicon)
 
         progressBar.visibility = View.VISIBLE
-    }
-
-    override fun onReceivedError(
-        view: WebView?,
-        request: WebResourceRequest?,
-        error: WebResourceError?
-    ) {
-        super.onReceivedError(view, request, error)
-
     }
 }
