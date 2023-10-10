cc34229ed28bb4146e19c13f40d9221e91244adb
kkingmo <55ooccmm@gmail.com>
2023-10-10 11:22:42 +0900

    default UI 구성

--- factory-2/VoiceRecorder/app/src/main/java/kingmo/kkk/voicerecorder/MainActivity.kt
+++ factory-2/VoiceRecorder/app/src/main/java/kingmo/kkk/voicerecorder/MainActivity.kt
@@ -2,10 +2,15 @@ package kingmo.kkk.voicerecorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kingmo.kkk.voicerecorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
