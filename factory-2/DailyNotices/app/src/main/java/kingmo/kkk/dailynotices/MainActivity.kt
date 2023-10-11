80927f60cbd92f015f0192b37a3781c258e7e71d
kkingmo <55ooccmm@gmail.com>
2023-10-11 15:34:22 +0900

    OKHttp를 이용한 통신 구성

--- factory-2/DailyNotices/app/src/main/java/kingmo/kkk/dailynotices/MainActivity.kt
+++ factory-2/DailyNotices/app/src/main/java/kingmo/kkk/dailynotices/MainActivity.kt
@@ -2,10 +2,32 @@ package kingmo.kkk.dailynotices
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = OkHttpClient()
+
+        val request: Request = Request.Builder().url("http://10.0.2.2:8080").build()
+
+        val callback = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("Client", e.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    Log.e("Client", "${response.body?.string()}")
+                }
+            }
+        }
+
+        client.newCall(request).enqueue(callback)
+
     }
 }
