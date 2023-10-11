a635f599b0b1952d48cfd8ef1f7dcfdd7a8ce7e0
kkingmo <55ooccmm@gmail.com>
2023-10-11 18:42:34 +0900

    user 불러오기 화면구성

--- factory-2/GitRepoSearch/app/src/main/java/kingmo/kkk/gitreposearch/MainActivity.kt
+++ factory-2/GitRepoSearch/app/src/main/java/kingmo/kkk/gitreposearch/MainActivity.kt
@@ -3,6 +3,9 @@ package kingmo.kkk.gitreposearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kingmo.kkk.gitreposearch.adapter.UserAdapter
+import kingmo.kkk.gitreposearch.databinding.ActivityMainBinding
 import kingmo.kkk.gitreposearch.model.Repo
 import kingmo.kkk.gitreposearch.model.UserDto
 import kingmo.kkk.gitreposearch.network.GithubService
@@ -13,9 +16,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create()).build()
@@ -30,14 +36,22 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        val userAdapter = UserAdapter()
+
+        binding.userRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = userAdapter
+        }
+
         githubService.searchUsers("squar").enqueue(object : Callback<UserDto> {
             override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
                 Log.e("MainActivity", "Search User : ${response.body().toString()}")
+
+                userAdapter.submitList(response.body()?.items)
             }
 
             override fun onFailure(call: Call<UserDto>, t: Throwable) {
             }
-
         })
     }
 }
