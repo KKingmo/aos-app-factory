331fd365f6568eca2b4975b951bbd969e0afb218
kkingmo <55ooccmm@gmail.com>
2023-10-11 16:50:50 +0900

    Gson 라이브러리 사용

--- factory-2/DailyNotices/app/src/main/java/kingmo/kkk/dailynotices/MainActivity.kt
+++ factory-2/DailyNotices/app/src/main/java/kingmo/kkk/dailynotices/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
+import com.google.gson.Gson
 import okhttp3.*
 import java.io.IOException
 
@@ -42,9 +43,11 @@ class MainActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         val response = response.body?.string()
 
+                        val message = Gson().fromJson(response, Message::class.java)
+
                         runOnUiThread {
                             informationTextView.isVisible = true
-                            informationTextView.text = response
+                            informationTextView.text = message.message
 
                             editText.isVisible = false
                             confirmButton.isVisible = false
