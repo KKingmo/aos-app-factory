94b3a4437a554e13b87a232d6b983046ee70866c
kkingmo <55ooccmm@gmail.com>
2023-10-16 18:20:39 +0900

    로그인, 회원가입 구현

--- factory-2/Chat/app/src/main/java/kingmo/kkk/chat/MainActivity.kt
+++ factory-2/Chat/app/src/main/java/kingmo/kkk/chat/MainActivity.kt
@@ -1,11 +1,23 @@
 package kingmo.kkk.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kingmo.kkk.chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val currentUser = Firebase.auth.currentUser
+        if (currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
