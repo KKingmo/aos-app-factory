55003c8a198305bb0eea6cf3e74fe4f3c8ab65ea
kkingmo <55ooccmm@gmail.com>
2023-10-17 11:42:26 +0900

    bottom Navigation 구성

--- factory-2/Chat/app/src/main/java/kingmo/kkk/chat/MainActivity.kt
+++ factory-2/Chat/app/src/main/java/kingmo/kkk/chat/MainActivity.kt
@@ -3,21 +3,51 @@ package kingmo.kkk.chat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kingmo.kkk.chat.databinding.ActivityMainBinding
+import kingmo.kkk.chat.userlist.UserFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val userFragment = UserFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val currentUser = Firebase.auth.currentUser
+
         if (currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.userList -> {
+                    replaceFragment(userFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.chatroomList -> {
+                    return@setOnItemSelectedListener true
+                }
+                R.id.myPage -> {
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    return@setOnItemSelectedListener false
+                }
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.frameLayout, fragment)
+                commit()
+            }
     }
 }
---
+++ factory-2/Chat/app/src/main/java/kingmo/kkk/chat/userlist/UserItem.kt
@@ -0,0 +1,7 @@
+package kingmo.kkk.chat.userlist
+
+data class UserItem(
+    val userId: String,
+    val username: String,
+    val description: String,
+)
