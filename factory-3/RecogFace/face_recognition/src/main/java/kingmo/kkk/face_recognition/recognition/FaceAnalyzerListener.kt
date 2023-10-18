65c52fb5cb9ef32fd2aed5af57e274c2ca1c5b64
kkingmo <55ooccmm@gmail.com>
2023-10-18 18:55:37 +0900

    mlkit 설치 얼굴 인식 모듈 구현 중

---
+++ factory-3/RecogFace/face_recognition/src/main/java/kingmo/kkk/face_recognition/recognition/FaceAnalyzerListener.kt
@@ -0,0 +1,18 @@
+package kingmo.kkk.face_recognition.recognition
+
+import android.graphics.PointF
+import android.graphics.RectF
+import android.util.SizeF
+
+interface FaceAnalyzerListener {
+
+    fun detect()
+
+    fun stopDetect()
+
+    fun notDetect()
+
+    fun detectProgress(progress: Float, message: String)
+
+    fun faceSize(rectF: RectF, sizeF: SizeF, pointF: PointF)
+}
---
+++ factory-3/RecogFace/face_recognition/src/main/java/kingmo/kkk/face_recognition/recognition/FaceAnalyzerStatus.kt
@@ -0,0 +1,5 @@
+package kingmo.kkk.face_recognition.recognition
+
+internal enum class FaceAnalyzerStatus {
+    Dectect, UnDetect, Smile, RightWink, LeftWink
+}
