da6e18212b192e6b3e0df7bc91c773d61c52a955
kkingmo <55ooccmm@gmail.com>
2023-10-18 17:50:32 +0900

    main UI 구성

--- factory-3/RecogFace/app/src/main/java/kingmo/kkk/recogface/MainActivity.kt
+++ factory-3/RecogFace/app/src/main/java/kingmo/kkk/recogface/MainActivity.kt
@@ -2,10 +2,20 @@ package kingmo.kkk.recogface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import kingmo.kkk.recogface.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+
+            startDetectButton.setOnClickListener {
+                it.isVisible = false
+            }
+        }
     }
 }
