1e04b08467ac3c7522d8bf69c089ef3430a66049
kkingmo <55ooccmm@gmail.com>
2023-10-20 13:41:50 +0900

    얼굴인식 모둘 구현 및 연동, 얼굴형 Mask UI구성

--- factory-3/RecogFace/face_recognition/src/main/java/kingmo/kkk/face_recognition/recognition/FaceAnalyzer.kt
+++ factory-3/RecogFace/face_recognition/src/main/java/kingmo/kkk/face_recognition/recognition/FaceAnalyzer.kt
@@ -1,6 +1,9 @@
 package kingmo.kkk.face_recognition.recognition
 
+import android.graphics.PointF
+import android.graphics.RectF
 import android.media.Image
+import android.util.SizeF
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
@@ -12,6 +15,7 @@ import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
+import kotlin.math.abs
 
 internal class FaceAnalyzer(
     lifecycle: Lifecycle,
@@ -21,6 +25,11 @@ internal class FaceAnalyzer(
 
     private var widthScaleFactor = 1F
     private var heightScaleFactor = 1F
+
+    private var preCenterX = 0F
+    private var preCenterY = 0F
+    private var preWidth = 0F
+    private var preHeight = 0F
 
     private val options = FaceDetectorOptions.Builder()
         .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
@@ -59,6 +68,7 @@ internal class FaceAnalyzer(
                 listener?.stopDetect()
                 detector.close()
             }
+            calDetectSize(face)
         } else if (detectStatus != FaceAnalyzerStatus.UnDetect && detectStatus != FaceAnalyzerStatus.Smile) {
             detectStatus = FaceAnalyzerStatus.UnDetect
             listener?.notDetect()
@@ -93,9 +103,47 @@ internal class FaceAnalyzer(
             }
     }
 
+    private fun calDetectSize(face: Face) {
+        val rect = face.boundingBox
+        val boxWidth = rect.right - rect.left
+        val boxHeight = rect.bottom - rect.top
+
+        val left = rect.right.translateX() - (boxWidth / 2)
+        val top = rect.top.translateY() - (boxHeight / 2)
+        val right = rect.left.translateX() + (boxWidth / 2)
+        val bottom = rect.bottom.translateY()
+
+        val width = right - left
+        val height = bottom - top
+        val centerX = left + width / 2
+        val centerY = top + height / 2
+
+        if (abs(preCenterX - centerX) > PIVOT_OFFSET
+            || abs(preCenterY - centerY) > PIVOT_OFFSET
+            || abs(preWidth - width) > SIZE_OFFSET
+            || abs(preHeight - height) > SIZE_OFFSET
+        ) {
+            listener?.faceSize(
+                RectF(left, top, right, bottom),
+                SizeF(width, height),
+                PointF(centerX, centerY)
+            )
+
+            preCenterX = centerX
+            preCenterY = centerY
+            preWidth = width
+            preHeight = height
+        }
+    }
+
+    private fun Int.translateX() = preview.width - (toFloat() * widthScaleFactor)
+    private fun Int.translateY() = toFloat() * heightScaleFactor
+
     companion object {
         private const val EYE_SUCCESS_VALUE = 0.1F
         private const val SMILE_SUCCESS_VALUE = 0.8F
-    }
 
+        private const val PIVOT_OFFSET = 15
+        private const val SIZE_OFFSET = 30
+    }
 }
